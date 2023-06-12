53d349d12d25c82f2b939c1dcd9cf3e512bad068
ssaavedraa <=>
2023-06-12 17:17:39 -0500

    migration to vite

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,10 +1,11 @@
+import logo from '../../assets/Firma-02.svg'
 import './footer.css'
-import React from 'react'
 
 export default function Footer(){
     return(
         <div className='footer'>
-            Developed by: Santiago Saavedra
+            Developed by:<img src={logo} alt="santiago-saavedra-logo" className="logo" width={300} height={90}/>
+
         </div>
     )
 }
--- src/components/searchBar/SearchBar.component.jsx
+++ src/components/searchBar/SearchBar.component.jsx
@@ -3,18 +3,27 @@ import React from "react";
 import { useState } from "react";
 
 export default function SearchBar({onSearch}){
-
     const [city, setCity] = useState('');
+    const [isFocused, setIsFocused] = useState(false);
 
-    function onSubmit(e){
+    function onSubmit(event){
+        event.preventDefault()
         onSearch(city)
         setCity('')
     }
 
+    function handleFocus (event) {
+        console.log(event)
+        setIsFocused(true)
+    }
+    function handleBlur () {
+        setIsFocused(false)
+    }
+
     return(
-        <div className="search-bar">
-            <input type="text" className="search-text" placeholder="Search city..." value={city} onChange={e => setCity(e.target.value)}/>
-            <span class="icon"><i class="fa fa-search" onClick={onSubmit} ></i></span>
-        </div>
+        <form className={`search-bar ${isFocused ? 'focus': ''}`} onSubmit={onSubmit}>
+            <input type="text" className="search-text" placeholder="Search city..." value={city} onChange={e => setCity(e.target.value)} onFocus={handleFocus} onBlur={handleBlur}/>
+            <span className="icon"><i className="fa fa-search" onClick={onSubmit} ></i></span>
+        </form>
     )
 }
